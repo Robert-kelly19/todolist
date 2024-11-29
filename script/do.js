13a4aab45c8ee6b6e9110672301b34fa8f9c22b4
kellyhandy31@gmail.com <kellyhandy31@gmail.comgit config --global user.name Robert-kelly19>
2024-11-29 14:26:15 +0100

    function

--- script/do.js
+++ script/do.js
@@ -1,9 +1,9 @@
-const input = document.getElementById("txt");
-const submit = document.getElementById("check");
-const list = document.getElementById("list");
+const input = document.getElementById('txt')
+const submit = document.getElementById('check')
+const list = document.getElementById('list')
 
-submit.addEventListener('click', ()=>{
-  let ulitem = document.createElement("li");
-  ulitem.innerHTML = input.value;
-  list.append(ulitem);
-});
+submit.addEventListener('click', () => {
+  const ulitem = document.createElement('li')
+  ulitem.innerHTML = input.value
+  list.append(ulitem)
+})
